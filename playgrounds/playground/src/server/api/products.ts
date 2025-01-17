87a0bc94ed4d8eacc331c6816f4b2d5c948f0122
Frederik Bußmann <frederik@bussmann.io>
2025-01-17 19:36:45 +0100

    chore: fixed playground
    Signed-off-by: Frederik Bußmann <frederik@bussmann.io>

--- playgrounds/playground/src/server/api/products.ts
+++ playgrounds/playground/src/server/api/products.ts
@@ -1,18 +1,25 @@
+import { z } from 'zod'
+
 export default defineCachedEventHandler(async (event) => {
+    const schema = z.object({
+        first: z.preprocess(Number, z.number()),
+    })
+
     const query = await getValidatedQuery(event, schema.parse)
+
     const storefront = useStorefront()
 
     return storefront.request(`#graphql
-    query FetchProducts($first: Int) {
-        products(first: $first) {
-            nodes {
-                id
-                title
-                description
-                descriptionHtml
+        query FetchProducts($first: Int) {
+            products(first: $first) {
+                nodes {
+                    id
+                    title
+                    description
+                    descriptionHtml
+                }
             }
         }
-    }
     `, {
         variables: query,
     })
