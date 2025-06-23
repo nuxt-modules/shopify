968b4f8ea70c2806c1470b5e4959604822ac0a5b
Frederik Bußmann <frederik@bussmann.io>
2025-06-23 18:13:02 +0200

    chore(template): fetch logo from shopify instead of static asset
    Signed-off-by: Frederik Bußmann <frederik@bussmann.io>

--- template/app/components/Logo.vue
+++ template/app/components/Logo.vue
@@ -1,5 +1,34 @@
 <script setup lang="ts">
 const localePath = useLocalePath()
+
+const storefront = useStorefront()
+
+const { data, error } = await useAsyncData('logo', async () => await storefront.request(`#graphql
+    query FetchLogo {
+        shop {
+            brand {
+                logo {
+                    image {
+                        ...ImageFields
+                    }
+                }
+            }
+        }
+    }
+    ${IMAGE_FRAGMENT}
+`), {
+    transform: data => data?.data?.shop?.brand?.logo,
+})
+
+if (error.value) {
+    throw createError({
+        statusCode: 500,
+        statusMessage: 'Failed to fetch logo',
+        data: error.value,
+    })
+}
+
+const logoUrl = computed(() => data.value?.image?.url)
 </script>
 
 <template>
@@ -8,7 +37,8 @@ const localePath = useLocalePath()
         class="flex items-center gap-3 mr-4 shrink-0"
     >
         <NuxtImg
-            src="/img/logo.png"
+            :src="logoUrl"
+            provider="shopify"
             class="h-5 w-auto light:invert"
             width="50"
             height="22"
