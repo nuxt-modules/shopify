eee0cb3126676a3511b86dfdc34bbf8ff73dc302
Frederik Bußmann <frederik@bussmann.io>
2025-08-31 12:05:36 +0200

    chore: temporarily remove template dependency
    Signed-off-by: Frederik Bußmann <frederik@bussmann.io>

--- template/nuxt.config.ts
+++ template/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: [
-        '@konkonam/nuxt-shopify',
+        '../src/module',
         '@nuxt/ui',
         '@nuxt/content',
         '@nuxtjs/i18n',
