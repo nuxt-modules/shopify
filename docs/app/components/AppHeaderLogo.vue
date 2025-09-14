c4df06596ce449ce78556f7b8a11f3e6d63d3bfc
Frederik Bußmann <frederik@bussmann.io>
2025-09-14 17:42:47 +0200

    chore: update documentation
    Signed-off-by: Frederik Bußmann <frederik@bussmann.io>

---
+++ docs/app/components/AppHeaderLogo.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex items-center gap-1">
+        <Icon
+            name="simple-icons-nuxt"
+            class="size-5.5 text-primary"
+        />
+
+        <p>
+            <span class="text-primary">Nuxt</span> Shopify
+        </p>
+    </div>
+</template>
