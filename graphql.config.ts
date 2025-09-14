2b61a3cebdd2e108caaa1f1bf61d0895be1bb31e
Frederik Bußmann <frederik@bussmann.io>
2025-09-15 00:01:48 +0200

    chore: update documentation
    Signed-off-by: Frederik Bußmann <frederik@bussmann.io>

--- graphql.config.ts
+++ graphql.config.ts
@@ -3,19 +3,10 @@ import type { IGraphQLConfig } from 'graphql-config'
 const config: IGraphQLConfig = {
     projects: {
         default: {
-            schema: './playgrounds/playground/.nuxt/schema/storefront.schema.json',
-            documents: [
-                './playgrounds/playground/**/*.{gql,graphql,js,ts,vue}',
-                '!./playgrounds/playground/**/*.admin.{gql,graphql,js,ts,vue}',
-                '!./playgrounds/playground/**/admin/**/*.{gql,graphql,js,ts,vue}',
-            ],
+            schema: './playgrounds/playground-v4/.nuxt/schema/storefront.schema.json',
         },
         admin: {
-            schema: './playgrounds/playground/.nuxt/schema/admin.schema.json',
-            documents: [
-                './playgrounds/playground/**/*.admin.{gql,graphql,js,ts,vue}',
-                './playgrounds/playground/**/admin/**/*.{gql,graphql,js,ts,vue}',
-            ],
+            schema: './playgrounds/playground-v4/.nuxt/schema/admin.schema.json',
         },
     },
 }
