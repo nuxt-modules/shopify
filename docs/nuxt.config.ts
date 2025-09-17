6a93923cf80c7afaa09408615e2a8692236c581d
Frederik Bußmann <frederik@bussmann.io>
2025-09-17 13:21:06 +0200

    chore: add docs favicon + webmanifest
    Signed-off-by: Frederik Bußmann <frederik@bussmann.io>

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -8,6 +8,22 @@ export default defineNuxtConfig({
         'motion-v/nuxt',
     ],
 
+    app: {
+        head: {
+            meta: [
+                { name: 'apple-mobile-web-app-title', content: 'Nuxt Shopify' },
+            ],
+            link: [
+                { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+                { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+                { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+                { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+                { rel: 'manifest', href: '/site.webmanifest' },
+            ],
+        },
+    },
+
     css: ['~/assets/css/main.css'],
 
     site: {
