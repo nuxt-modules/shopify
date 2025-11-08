6a46a125a011992023771e5adf3b072ca720af6c
Frederik Bußmann <frederik@bussmann.io>
2025-11-08 14:12:31 +0100

    chore: update docs, template
    Signed-off-by: Frederik Bußmann <frederik@bussmann.io>

--- docs/app/app.config.ts
+++ docs/app/app.config.ts
@@ -1,4 +1,8 @@
 export default defineAppConfig({
+    seo: {
+        description: 'Easily integrate Shopify into your Nuxt 3 & 4 app. Build powerful e-commerce applications with Nuxt Shopify.',
+    },
+
     socials: {
         x: 'https://go.nuxt.com/x',
         discord: 'https://go.nuxt.com/discord',
