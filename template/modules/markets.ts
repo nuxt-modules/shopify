39e8c04714c16ba51d5dfebe25c3b7851037391f
Frederik Bußmann <frederik@bussmann.io>
2026-08-13 09:50:59 +0200

    chore(template): fix auto-i18n markets setup
    Signed-off-by: Frederik Bußmann <frederik@bussmann.io>

--- template/modules/markets.ts
+++ template/modules/markets.ts
@@ -1,5 +1,5 @@
-import { defineNuxtModule } from '@nuxt/kit'
-
+import { resolve } from 'node:path'
+import { defineNuxtModule } from 'nuxt/kit'
 import { createStorefrontClient } from '@nuxtjs/shopify'
 
 const toLocale = (language: string, country: string) => `${language}-${country}`.toLowerCase()
@@ -31,14 +31,20 @@ export default defineNuxtModule({
 
     const { country, language, availableCountries } = data.localization
 
-    nuxt.options.i18n.defaultLocale = toLocale(language.isoCode, country.isoCode)
-
-    nuxt.options.i18n.locales = availableCountries.flatMap(country =>
+    const locales = availableCountries.flatMap(country =>
       country.availableLanguages.map(language => ({
         code: toLocale(language.isoCode, country.isoCode),
         language: `${language.isoCode.toLowerCase()}-${country.isoCode}`,
         name: language.endonymName,
         file: `${language.isoCode.toLowerCase()}.json`,
       })))
+
+    nuxt.options.i18n.defaultLocale = toLocale(language.isoCode, country.isoCode)
+    nuxt.options.i18n.locales = locales
+
+    nuxt.hook('i18n:registerModule', register => register({
+      langDir: resolve(nuxt.options.rootDir, 'i18n/locales'),
+      locales,
+    }))
   },
 })
