bfca878e2cf402565fb8239eef8ec1c6c961dbe0
Frederik Bußmann <frederik@bussmann.io>
2026-08-16 10:57:33 +0200

    feat: add h3 event to server side storefront client
    Signed-off-by: Frederik Bußmann <frederik@bussmann.io>

--- src/runtime/server/utils/storefront/client.ts
+++ src/runtime/server/utils/storefront/client.ts
@@ -1,15 +1,18 @@
 import type { StorefrontApiClient } from '@nuxtjs/shopify/storefront'
+import type { H3Event } from 'h3'
 
 import { useNitroApp } from 'nitropack/runtime'
 import { useRuntimeConfig } from '#imports'
 import { createStorefrontClient } from '../../../utils/clients/storefront'
 
-export function useStorefront(): StorefrontApiClient {
+export function useStorefront(event?: H3Event): StorefrontApiClient {
   const { _shopify } = useRuntimeConfig()
 
   const nitroApp = useNitroApp()
 
   return createStorefrontClient(_shopify!, {
+    event,
+
     onConfigure: params => nitroApp.hooks.callHook('storefront:client:configure', params),
     onCreate: params => nitroApp.hooks.callHook('storefront:client:create', params),
     onRequest: params => nitroApp.hooks.callHook('storefront:client:request', params),
